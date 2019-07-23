991447b08ce12dada7c77ccd71d06ff6a812f913
Amal Joseph <amal@gmail.com>
2019-07-23 11:35:24 +0530

    third update

--- flaskdemo/first.py
+++ flaskdemo/first.py
@@ -1,5 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 app = Flask(__name__)
+
+
+@app.route('/insertion/',methods=['POST'])
+def insert():
+   print("running")
+   result = request.form
+   print result
+   return render_template('next.html')
+
+@app.route('/nextpage/')
+def nextpg():
+   return render_template('next.html')
+
+@app.route('/details/<string:data>')
+def details(data):
+   print(data)
+   return data
 
 @app.route('/')
 def result():
